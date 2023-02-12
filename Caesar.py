a9ec7e548fbd9e84696f54179d45b26a5a95872f
MugiranezaJ <jackswalter7@gmail.com>
2023-02-12 20:43:15 +0200

    implementation of caesar

---
+++ Caesar.py
@@ -0,0 +1,32 @@
+
+ALPHABET = ' ABCDEFGHIJKLMNOPQRSTUVWXYZ!'
+KEY = 3
+
+
+def caesar_encrypt(plain_text):
+    cipher_text = ''
+    plain_text = plain_text.upper()
+
+    for c in plain_text:
+        index = ALPHABET.find(c)
+        index = (index + KEY) % len(ALPHABET)
+        cipher_text = cipher_text + ALPHABET[index]
+    return cipher_text
+
+
+def caesar_decrypt(cipher_text):
+    plain_text = ''
+
+    for c in cipher_text:
+        index = ALPHABET.find(c)
+        index = (index - KEY) % len(ALPHABET)
+        plain_text = plain_text + ALPHABET[index]
+    return plain_text
+
+
+if __name__ == '__main__':
+    m = 'welcome to my UDEMY course!'
+    encrypted = caesar_encrypt(m)
+
+    print(encrypted)
+    print(caesar_decrypt(encrypted))
